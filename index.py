c2974ef3fe3c93ff8bdbb0e2c341cbe06434d52e
Fábio <fabiomt92@hotmail.com>
2021-01-26 21:08:45 -0300

    1

--- index.py
+++ index.py
@@ -5,26 +5,12 @@ from flask import Flask, make_response, request, render_template
 app = Flask(__name__)
 
 
-# Nome dos ativos no site do infomoney
-def ativos_lista():
-    r = requests.get('https://www.infomoney.com.br/cotacoes/empresas-b3/')
-    s = bs(r.content, 'html.parser')
-    a=s.findAll('tr')
-    a=[[i.find('td', class_='higher'), i.findAll('td', class_='strong')] for i in a[1:]]
-    a=[[i[0].text, [j.find('a').text.strip() for j in i[1]]]for i in a if any(i)]
-    a=[[[ei, t] for ei in e] for t, e in zip([i[0] for i in a], [i[1] for i in a])]
-    a=[j for i in a for j in i]
-    return [i for i in a if (len(i[0])==5 and i[0][-1:] in ('3','4','5')) or (len(i[0])==6 and i[-2:]=='11')]
-    
-    
-
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
-app.run()
 
 
 
